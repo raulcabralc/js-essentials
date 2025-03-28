c54244da72519982fdf5a54d14045d84c4fa9d5a
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-03-27 23:03:12 -0300

    Fetch Axios

---
+++ aula93 fetch axios/script.js
@@ -0,0 +1,37 @@
+// fetch('pessoas.json')
+//     .then((data) => {
+//         return data.json();
+//     })
+//     .then((json) => {
+//         return loadElements(json)
+//     })
+//     .catch(error => console.log('Erro!', error));
+
+axios('pessoas.json')
+    .then(response => loadElements(response.data))
+    .catch(error => console.log('Erro!', error));
+
+function loadElements(json) {
+    const table = document.createElement('table');
+    const result = document.querySelector('.result');
+
+    for (let person of json) {
+        const tr = document.createElement('tr');
+        let td = document.createElement('td');
+
+        td.innerHTML = person.nome;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = person.sexo;
+        tr.appendChild(td);
+
+        td = document.createElement('td');
+        td.innerHTML = person.idade;
+        tr.appendChild(td);
+
+        table.appendChild(tr);
+    }
+
+    result.appendChild(table);
+}
