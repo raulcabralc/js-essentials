e18b63da62ee80aead022060594b22fcea15a913
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-03-30 16:42:34 -0300

    Webpack

---
+++ aula95 webpack/public/assets/js/bundle.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!***********************!*\
+  !*** ./src/script.js ***!
+  \***********************/
+/*
+npm i --save-dev @babel/core @babel/cli @babel/preset-env babel-loader webpack webpack-cli regenerator -runtime core-js@2
+*/
+
+// NUNCA MEXA NA PASTA JS NEM NO BUNDLE.JS, APENAS NO CODIGO EM SRC
+
+console.log('Teste!!!');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ aula95 webpack/src/script.js
@@ -0,0 +1,7 @@
+/*
+npm i --save-dev @babel/core @babel/cli @babel/preset-env babel-loader webpack webpack-cli regenerator -runtime core-js@2
+*/
+
+// NUNCA MEXA NA PASTA JS NEM NO BUNDLE.JS, APENAS NO CODIGO EM SRC
+
+console.log('Teste!!!');
