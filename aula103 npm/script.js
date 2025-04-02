7a6bf3aef6a27f8866194ac32d68a73e6f142beb
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-04-02 17:19:37 -0300

    Npm

---
+++ aula103 npm/script.js
@@ -0,0 +1,14 @@
+/*
+
+npm install ... (major.x -> instala a versão mais recente da major) (-E -> trava na versão) (instala algo)
+npm init (-y -> versão menor) (gera o package.json)
+npm ls (lista os pacotes)
+npm update (atualiza os pacotes)
+versão: 5.     0.     0
+        major  minor  patch
+^ significa que atualizará sempre a minor e patch
+~ significa que atualizará sempre a patch
+npm outdated (verifica os pacotes desatualizados)
+npm uninstall ... (desinstala o pacote)
+
+*/
