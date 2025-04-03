69d5bfd1c3068213b51119569bcd00690a33d495
Raul Cabral Caxeta <raulcabralc@gmail.com>
2025-04-03 13:55:50 -0300

    Express

---
+++ aula106 express/server.js
@@ -0,0 +1,28 @@
+// CRUD -> Create  Read  Update  Delete
+//         POST    GET   PUT     DELETE
+
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response) => {
+    // app.get('rota', função(requisição, resposta))
+    response.send(`
+        <form action="/" method="POST" />
+        Name: <input type="text" name="name" />
+        <button>Submit</button>
+        </form>
+        `);
+});
+
+app.post('/', (request, response) => {
+    response.send('Formulary received');
+})
+
+app.get('/contact', (request, response) => {
+    response.send('raulcabralc@gmail.com');
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000.');
+    console.log('http://localhost:3000');
+});
